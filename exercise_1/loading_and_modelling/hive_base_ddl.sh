92ba64f872dcb7dc77b68f2ecb44f790e4b93257
dswald <dswald@gmail.com>
2016-10-08 15:18:52 -0700

    added backticks

--- exercise_1/loading_and_modelling/hive_base_ddl.sh
+++ exercise_1/loading_and_modelling/hive_base_ddl.sh
@@ -3,10 +3,10 @@
 #DDL to create hostpitals
 DROP TABLE hospitals;
 CREATE EXTERNAL TABLE hospitals (
-  provider_id INT,
-  hospital_name STRING,
-  hospital_address STRING,
-  hospital_state STRING
+  `provider_id` INT,
+  `hospital_name` STRING,
+  `hospital_address` STRING,
+  `hospital_state` STRING
   )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
@@ -20,10 +20,10 @@ LOCATION '/home/w205/hospital_compare';
 #DDL to create measures
 DROP TABLE measures;
 CREATE EXTERNAL TABLE measures (
-  measure_id INT,
-  measure_name STRING,
-  measure_startdate DATE,
-  measure_enddate DATE
+  `measure_id` INT,
+  `measure_name` STRING,
+  `measure_startdate` DATE,
+  `measure_enddate` DATE
   )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
@@ -37,12 +37,12 @@ LOCATION '/home/w205/hospital_compare';
 #DDL to create effective_care
 DROP TABLE effective_care;
 CREATE EXTERNAL TABLE measures (
-  provider_id INT,
-  measure_id INT,
-  care_score INT,
-  care_sample STRING,
-  care_start STRING,
-  care_end STRING
+  `provider_id` INT,
+  `measure_id` INT,
+  `care_score` INT,
+  `care_sample` STRING,
+  `care_start` STRING,
+  `care_end` STRING
   )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
@@ -56,14 +56,14 @@ LOCATION '/home/w205/hospital_compare';
 #DDL to create readmissions
 DROP TABLE readmissions;
 CREATE EXTERNAL TABLE measures (
-  provider_id INT,
-  measure_id INT,
-  readmission_score FLOAT,
-  readmission_low FLOAT,
-  readmission_high FLOAT,
-  readmission_sample STRING,
-  readmission_start STRING,
-  readmission_end STRING
+  `provider_id` INT,
+  `measure_id` INT,
+  `readmission_score` FLOAT,
+  `readmission_low` FLOAT,
+  `readmission_high` FLOAT,
+  `readmission_sample` STRING,
+  `readmission_start` STRING,
+  `readmission_end` STRING
   )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
@@ -77,9 +77,9 @@ LOCATION '/home/w205/hospital_compare';
 #DDL to create survey
 DROP TABLE survey;
 CREATE EXTERNAL TABLE measures (
-  provider_id INT,
-  survey_baseline INT,
-  survey_consistency INT,
+  `provider_id` INT,
+  `survey_baseline` INT,
+  `survey_consistency` INT,
   )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
