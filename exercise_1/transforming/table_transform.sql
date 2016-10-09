486b0651e175db33c0864eb770c676a8b569a9d0
dswald <dswald@gmail.com>
2016-10-09 08:20:43 -0700

    initial commit of table_transform.sql

---
+++ exercise_1/transforming/table_transform.sql
@@ -0,0 +1,50 @@
+#CTAS commands
+
+CREATE TABLE hospitals_lite AS SELECT
+provider_id,
+hospital_name,
+hospital_address,
+hospital_city,
+hospital_state,
+hospital_zip FROM hospitals;
+
+
+CREATE TABLE measures_lite AS SELECT
+measure_id,
+measure_name,
+measure_startdate,
+measure_enddate
+FROM measures;
+
+
+CREATE TABLE effective_lite AS SELECT
+provider_id,
+measure_id,
+care_score,
+care_start,
+care_end
+FROM effective_care;
+
+
+CREATE TABLE readmissions_lite AS SELECT
+provider_id,
+measure_id,
+readmission_score,
+readmission_sample,
+readmission_low,
+readmission_high,
+readmission_start,
+readmission_end
+FROM readmissions;
+
+
+CREATE TABLE survey_lite AS SELECT
+provider_id,
+measure_id,
+readmission_score,
+readmission_sample,
+readmission_low,
+readmission_high,
+readmission_start,
+readmission_end
+FROM survey;
