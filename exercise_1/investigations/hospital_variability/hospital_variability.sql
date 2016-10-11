70fd8204a78597cd94e3fdcb770498703307af50
dswald <dswald@gmail.com>
2016-10-10 17:46:43 -0700

    remove exercise_1 so that I can properly submit

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++
@@ -1,5 +0,0 @@
-SELECT provider_id, MAX(score) - MIN(score) AS score_variation FROM best_hospitals
-WHERE score IS NOT NULL
-GROUP BY provider_id
-ORDER BY score_variation DESC
-LIMIT 10;
--- exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
+++
@@ -1,3 +0,0 @@
-SELECT CORR(sl.survey_baseline, best.score)
-FROM survey_lite sl, best_hospitals best
-WHERE best.score IS NOT NULL AND sl.provider_id = best.provider_id;
