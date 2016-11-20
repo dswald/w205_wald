1ececcbebd6756b421bab6fb9d55190343ad35ca
Dan Wald <dswald@gmail.com>
2016-11-20 12:51:58 -0800

    modify wordcount to open DB in initialization - assuming new DB every time.

--- EX2/EX2Tweetwordcount/src/bolts/wordcount.py
+++ EX2/EX2Tweetwordcount/src/bolts/wordcount.py
@@ -2,31 +2,29 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-from Twittercredentials import *
+#from Twittercredentials import *
 import psycopg2
-
-conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
-
-#Create a Table
-#The first step is to create a cursor.
-
-cur = conn.cursor()
-cur.execute('''CREATE TABLE Tweetwordcount
-       (word TEXT PRIMARY KEY     NOT NULL,
-       count INT     NOT NULL);''')
-conn.commit()
-conn.close()
-
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
 
+        conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+        #Create a Table
+        #The first step is to create a cursor.
+
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE Tweetwordcount
+               (word TEXT PRIMARY KEY     NOT NULL,
+               count INT     NOT NULL);''')
+        conn.commit()
+        conn.close()
 
     def process(self, tup):
         word = tup.values[0]
-        
+
 
         cur = conn.cursor()
 
@@ -40,21 +38,19 @@ class WordCounter(Bolt):
         cur.execute("SELECT word, count from Tweetwordcount")
         records = cur.fetchall()
         num_word = 0;
-        for rec in records:
-           num_word = rec[1] + self.count[word];
+        if rec in records:
+           num_word = rec[1] + 1;
            #update the count
            cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s", (word, num_word))
            conn.commit()
-
+       else:
+           self.counts[word] += 1
+           # self.emit([word, self.counts[word]])
+           cur.execute("INSERT INTO Tweetwordcount (word,self.counts[word]) \
+                 VALUES (word, 1)");
+           conn.commit()
         conn.commit()
-
-        # Increment the local count with a new insertion
-        if num_word = 0:
-            self.counts[word] += 1
-            # self.emit([word, self.counts[word]])
-            cur.execute("INSERT INTO Tweetwordcount (word,self.counts[word]) \
-                  VALUES (word, 1)");
-            conn.commit()
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
+        conn.close()
