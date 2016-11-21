cd6c9e3b1f4cff172a4fbcc8ae06b4e72f3e46e3
Dan Wald <dswald@gmail.com>
2016-11-20 17:20:22 -0800

    working with counts

--- EX2/EX2Tweetwordcount/src/bolts/wordcount.py
+++ EX2/EX2Tweetwordcount/src/bolts/wordcount.py
@@ -17,14 +17,14 @@ class WordCounter(Bolt):
 
         #Drop table and create new instance for every execution.
         #I made a design decision to support streaming analytics and not a running total.
-        cur = conn.cursor()
-        cur.execute('''DROP TABLE Tweetwordcount''')
-        conn.commit()
-        cur.execute('''CREATE TABLE Tweetwordcount
-               (word TEXT PRIMARY KEY     NOT NULL,
-               count INT     NOT NULL);''')
-        conn.commit()
-        conn.close()
+        # cur = conn.cursor()
+        # cur.execute('''DROP TABLE Tweetwordcount''')
+        # conn.commit()
+        # cur.execute('''CREATE TABLE Tweetwordcount
+        #        (word TEXT PRIMARY KEY     NOT NULL,
+        #        count INT     NOT NULL);''')
+        # conn.commit()
+        # conn.close()
 
 
     def process(self, tup):
